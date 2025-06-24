4fa219225589e535d1c6e335ae58ae2b46495d06
Priya Sharma <priyasharma2702@gmail.com>
2025-06-24 18:35:27 +0200

    added some code for client side navigation, it has some issues with server side nagivation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const FETCH_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [posts, setPosts] = useState([]);
+	const [totalPosts, setTotalPosts] = useState(0);
+	const postsPerPage = 10;
+	const [currentPage, setCurrentPage] = useState(1);
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+	const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost + 1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	//client side pagination
+	// useEffect(() => {
+	// 	const fetchPosts = async () => {
+	// 		const response = await fetch(FETCH_URL);
+	// 		const json = await response.json();
+	// 		setPosts(json);
+	// 	};
+	// 	fetchPosts();
+	// }, []);
+
+	//server side pagination
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(
+				`${FETCH_URL}?_page=${currentPage}&_limit=${postsPerPage}`
+			);
+			const json = await response.json();
+
+			const totalPosts = response.headers.get('X-Total-Count');
+			setPosts(json);
+			setTotalPosts(Number(totalPosts));
+		};
+		fetchPosts();
+	}, [currentPage, postsPerPage]);
+
+	return (
+		<div className='root'>
+			<h1>Blogs</h1>
+			<PostList posts={currentPosts} />
+			<Pagination
+				currentPage={currentPage}
+				setCurrentPage={setCurrentPage}
+				// totalPages={posts.length / postsPerPage}
+				totalPages={totalPosts / postsPerPage}
+			/>
+		</div>
+	);
 }
 
-export default App
+function PostList({ posts }) {
+	return (
+		<ul className='post-list'>
+			{posts.map((post) => (
+				<li key={post.id}>
+					<h2>{post.title}</h2>
+					<p>{post.body}</p>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+function Pagination({ totalPages, currentPage, setCurrentPage }) {
+	const handlePageClick = (page) => {
+		setCurrentPage(page);
+	};
+	return (
+		<nav>
+			<ul className='pagination'>
+				{Array.from({ length: totalPages }, (v, i) => i).map((page) => {
+					return (
+						<li
+							key={page}
+							className={`page-item ${currentPage === page ? 'active' : ''}`}>
+							<button onClick={() => handlePageClick(page + 1)}>
+								{page + 1}
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+}
+export default App;
